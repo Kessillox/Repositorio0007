a8ad25b19cc557c1bbce37cfb5cf00734ba7e684
cokardox <ofernandez.app@gmail.com>
2023-05-30 23:19:41 -0400

    subiendo individual oscar

---
+++ src/Individual_Oscar/Ejercicio23.java
@@ -0,0 +1,40 @@
+package Individual_Oscar;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Ejercicio23 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        String resultado = "";
+
+        do {
+            System.out.println("ingrese una cadena");
+            cadena = sc.nextLine();
+        } while (cadena.isEmpty());
+
+
+        for(int i=0; i < cadena.length(); i++){
+            char letra = cadena.charAt(i);
+            if (letra == ' ') {
+                continue;
+
+            }else if (Character.isLowerCase(letra)) {
+                letra = Character.toUpperCase(letra);
+
+            }else if (Character.isUpperCase(letra)){
+                letra = Character.toLowerCase(letra);
+            }
+
+
+
+            resultado += letra;
+
+        }
+
+
+        System.out.println("El resultado es: " + resultado);
+
+
+}
