2633efbe237e946f2a7ae51b385a517abaab6aad
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-05-31 10:33:57 -0400

    Instancias de clase y metodo toString()

---
+++ src/Cliente.java
@@ -0,0 +1,81 @@
+public class Cliente {
+
+    private String rut;
+    private String nombreEmpresa;
+    private String nombreRepresentante;
+    private String telefonoRepresentante;
+    private String direccion;
+    private String comuna;
+
+    public Cliente() {
+    }
+
+    public Cliente(String rut, String nombreEmpresa, String nombreRepresentante, String telefonoRepresentante, String direccion, String comuna) {
+        this.rut = rut;
+        this.nombreEmpresa = nombreEmpresa;
+        this.nombreRepresentante = nombreRepresentante;
+        this.telefonoRepresentante = telefonoRepresentante;
+        this.direccion = direccion;
+        this.comuna = comuna;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getNombreRepresentante() {
+        return nombreRepresentante;
+    }
+
+    public void setNombreRepresentante(String nombreRepresentante) {
+        this.nombreRepresentante = nombreRepresentante;
+    }
+
+    public String getTelefonoRepresentante() {
+        return telefonoRepresentante;
+    }
+
+    public void setTelefonoRepresentante(String telefonoRepresentante) {
+        this.telefonoRepresentante = telefonoRepresentante;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "rut='" + rut + '\'' +
+                ", nombreEmpresa='" + nombreEmpresa + '\'' +
+                ", nombreRepresentante='" + nombreRepresentante + '\'' +
+                ", telefonoRepresentante='" + telefonoRepresentante + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", comuna='" + comuna + '\'' +
+                '}';
+    }
+}
---
+++ src/Usuario.java
@@ -0,0 +1,47 @@
+public class Usuario {
+    private String nombre;
+    private String fechaDeNacimiento;
+    private String run;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String fechaDeNacimiento, String run) {
+        this.nombre = nombre;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.run = run;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", fechaDeNacimiento='" + fechaDeNacimiento + '\'' +
+                ", run='" + run + '\'' +
+                '}';
+    }
+}
