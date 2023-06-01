0e40c4944d6ebd66fa3a7f602587e06aa365a198
Manpif <manuel.pinot97@gmail.com>
2023-06-01 12:03:42 -0400

    TI25

---
+++ src/individuales/Main.java
@@ -0,0 +1,18 @@
+package individuales;
+
+import javax.swing.JOptionPane;
+import individuales.Trabajador;
+public class Main {
+
+        public static void main(String[] args) {
+            Trabajador trabajador = new Trabajador(
+                    "Manuel Fernando",
+                    "Pinot Friz",
+                    19555544-3,
+                    944468294,
+                    25);
+
+            System.out.println(trabajador);
+        }
+    }
+
