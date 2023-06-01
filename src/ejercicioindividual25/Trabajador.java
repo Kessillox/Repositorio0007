34674247d5719004644371a70339301cce196fe2
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-06-01 10:17:21 -0400

    Eliminando el Main

--- src/ejercicioindividual25/Trabajador.java
+++ src/ejercicioindividual25/Trabajador.java
@@ -42,3 +42,18 @@ public class Trabajador {
         return Integer.parseInt(numeroStr);
     }
 }
+
+//Pruebas en caso de tener un Main
+
+/*
+* Trabajador luis = new Trabajador(
+                "Luis Alejandro\u0020",
+                "Zambrano Salazar",
+                "18.685.014-9",
+                "+56998428164",
+                28
+        );
+
+        System.out.println(luis.nombreCompleto());
+        System.out.println(luis.descomponerRun());
+* */
