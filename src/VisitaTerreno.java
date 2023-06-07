a9f3e696b9b69da4608e206de4f158aa960105bf
dayhip-ti <dayhip.ti@gmail.com>
2023-06-07 11:49:08 -0400

    visita terreno

--- src/VisitaTerreno.java
+++ src/VisitaTerreno.java
@@ -1,2 +1,11 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class VisitaTerreno {
+    private int idTerreno;
+    private LocalDate diaTerreno;
+    private LocalDateTime horaTerreno;
+    private String lugarTerreno;
+    private String comentarios;
+
 }
