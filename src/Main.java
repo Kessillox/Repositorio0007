5f0a2b602c079191e9263a7f979fc2d7c7fe784d
dayhip-ti <dayhip.ti@gmail.com>
2023-06-07 17:14:58 -0400

    Individual 25

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import Individual25.Trabajador;
+
 public class  Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Trabajador trabajador1= new Trabajador("Pancho",
+                "Burga","12.000.478-5","555555",32);
+
+
+
+        System.out.println("El rut es: "+trabajador1.descomponerRun());
+        System.out.println("El nombre completo es: "+trabajador1.nombreCompleto());
     }
 }
