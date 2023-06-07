1cb8d52089f54e68a5c905464e9baca429504ef8
Livio <livio.gutierrez.v@gmail.com>
2023-06-07 16:01:48 -0400

    verificacion de titulo

--- src/sprint/Principal.java
+++ src/sprint/Principal.java
@@ -1,12 +1,16 @@
 package sprint;
 
+import sprint.entity.Profesional;
+
 import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+
         //VARIABLES
         int op =0;
+        String titulo ="";
 
         System.out.println("Bienvenido, ¿en que perfil desea trabajar?");
         System.out.println("------Perfiles------");
@@ -24,13 +28,23 @@ public class Principal {
             }
             switch (op){
                 case 1:
-                    System.out.println("Perfil Cliente");
+                    System.out.println("------Perfil Cliente------");
                     break;
                 case 2:
-                    System.out.println("Perfil Profesional");
+                    Scanner scP = new Scanner(System.in);
+                    System.out.println("------Perfil Profesional------");
+                    System.out.println("Ingrese su titulo");
+
+                    Profesional profesional = new Profesional("Livio", "Gutierrez", "Franchesco", "05/12/1999", "92.666.666", scP.nextLine(),"15/12/09");
+                    if (profesional.getTitulo() != null && profesional.getTitulo().length() > 10 && profesional.getTitulo().length() < 50) {
+                        System.out.println(profesional.getTitulo());
+                    }else {
+                        System.out.println("Error: El titulo debe tener entre 10 y 50 caracteres.");
+                    }
+
                     break;
                 case 3:
-                    System.out.println("Perfil Administrativo");
+                    System.out.println("------Perfil Administrativo------");
                     break;
             }
         }while (op>3);
