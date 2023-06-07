43c9125a848363c45ac4e3daec89440e1b4f7477
IgRetamalC <igretamalc@gmail.com>
2023-06-07 17:59:52 -0400

    Administrativo y Profesional

--- src/Administrativo.java
+++ src/Administrativo.java
@@ -1,4 +1,25 @@
-
 public class Administrativo {
+    private String area;
+    private String experienciaPrevia;
 
+    @Override
+    public String toString() {
+        return "Administrativo{" +
+                "area='" + area + '\'' +
+                ", experienciaPrevia='" + experienciaPrevia + '\'' +
+                '}';
+    }
+
+    public Administrativo(String area, String experienciaPrevia) {
+        this.area = area;
+        this.experienciaPrevia = experienciaPrevia;
+    }
+    public String getArea(){ return area; }
+    public void setArea(String area) {
+        this.area = area;
+    }
+    public String getExperienciaPrevia(){ return experienciaPrevia; }
+    public void setExperienciaPrevia() {
+        this.experienciaPrevia = experienciaPrevia;
+    }
 }
--- src/Profesional.java
+++ src/Profesional.java
@@ -1,2 +1,33 @@
+import java.time.LocalDate;
+
 public class Profesional {
+    private String titulo;
+    private LocalDate fechaDeIngreso;
+    @Override
+    public String toString() {
+        return "Profesional{" +
+                "titulo='" + titulo + '\'' +
+                "fechaDeIngreso='" + fechaDeIngreso + '\'' +
+                '}';
+    }
+public Profesional(){
+}
+    public Profesional(String titulo, LocalDate fechaDeIngreso) {
+        this.titulo = titulo;
+        this.fechaDeIngreso = fechaDeIngreso;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public LocalDate getFechaDeIngreso() {
+        return fechaDeIngreso;
+    }
+
+    public void setFechaDeNacimientoUsuario(LocalDate fechaDeNacimientoUsuario) {
+        this.fechaDeIngreso = fechaDeNacimientoUsuario;
+    }
 }
