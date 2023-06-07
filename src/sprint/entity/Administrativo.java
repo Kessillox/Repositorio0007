81a7d6f5cd62f39a74410a29f6ac2984150e8908
Livio <livio.gutierrez.v@gmail.com>
2023-06-07 11:07:11 -0400

    administrativo

--- src/sprint/entity/Administrativo.java
+++ src/sprint/entity/Administrativo.java
@@ -1,4 +1,7 @@
 package sprint.entity;
 
 public class Administrativo extends Usuario{
+    private String area;
+    private String experienciaPrevia;
+
 }
--- src/sprint/entity/Cliente.java
+++ src/sprint/entity/Cliente.java
@@ -1,7 +1,6 @@
 package sprint.entity;
 
 public class Cliente extends Usuario{
-
     int casa;
     int perro;
     int numeros;
