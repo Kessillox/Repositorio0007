47de94e9edf6e10c19cc99d6dcf956ba2043ed05
dayhip-ti <dayhip.ti@gmail.com>
2023-06-07 12:02:10 -0400

    javadoc

--- src/VisitaTerreno.java
+++ src/VisitaTerreno.java
@@ -1,6 +1,11 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+/**
+ * Clase que almacena la información visita terreno
+ * @author Dayanna Nuñez
+ * @version 1.0
+ */
 public class VisitaTerreno {
     private int idTerreno;
     private LocalDate diaTerreno;
