9cc296a78f3b038fd5d4fae054043d3587e131d2
Cristian.Trureo <c.trureo87@gmail.com>
2023-06-07 11:35:59 -0400

    del grupal 17 principio solid de segregacion de interfaces

--- src/empresaclase14/IAsesoria.java
+++ src/empresaclase14/IAsesoria.java
@@ -1,13 +1,13 @@
 package empresaclase14;
 /**
- * Esta interfaz aplica el principio de Segregación de la Interfaz, ya que todos los clientes
+ * Esta interfaz aplica el principio de Segregación de interfaces, ya que todos los clientes
  * que implementen esta interfaz utilizarán el método 'analizarUsuario()', y no
  * dependen de métodos que no utilicen.
  */
 public interface IAsesoria {
     /**
      * Muestra nombre y datos específicos de cada tipo de usuario.
-     * Este método debe ser implementado por todas las clases que implementen esta interfaz.
+     * Este método está siendo implementado por todas las clases que implementen esta interfaz.
      */
     public void analizarUsuario();
 }
