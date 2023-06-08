57645cc37f00ec0dc3090b235215ca6f08dd34f7
Cristian.Trureo <c.trureo87@gmail.com>
2023-06-08 10:38:22 -0400

    renombre a validarFecha

--- src/empresaclase14/validaciones/validacioncapacitacion/ValidacionCapacitacion.java
+++ src/empresaclase14/validaciones/validacioncapacitacion/ValidacionCapacitacion.java
@@ -1,4 +1,11 @@
 package empresaclase14.validaciones.validacioncapacitacion;
 
 public class ValidacionCapacitacion {
+
+
+    // validacion ID está en validaciones.validacionidentificador
+
+    // validacion RUT está en validaciones.validacioncliente...
+
+
 }
