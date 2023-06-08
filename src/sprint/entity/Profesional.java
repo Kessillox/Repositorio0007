343ffed67a303705d610b977a7ad69a48ecb8698
Genesis Paola <gnsspena3@gmail.com>
2023-06-08 09:47:31 -0400

    solucionando

--- src/sprint/entity/Profesional.java
+++ src/sprint/entity/Profesional.java
@@ -4,7 +4,7 @@ public class Profesional extends Usuario{
     private String titulo;
     private String fechaDeIngreso;
 
-    public Profesional(String nombre, String apellido1, String apellido2, String fechaNacimiento, String run, String titulo, String fechaDeIngreso) {
+    public Profesional(String nombre, String apellido1, String apellido2, String fechaNacimiento, int run, String titulo, String fechaDeIngreso) {
         super(nombre, apellido1, apellido2, fechaNacimiento, run);
         this.titulo = titulo;
         this.fechaDeIngreso = fechaDeIngreso;
