54e3b00cbbe6e16a02937db35898e915639576fa
IvanBecerraA <ivanbecerra91@gmail.com>
2023-06-08 13:23:58 -0400

    Se crea la clase AlmacenarClienteController para las validaciones (sin funcionalidad)

---
+++ src/cl/bootcamp/empresa/controllers/AlmacenarClienteController.java
@@ -0,0 +1,13 @@
+package cl.bootcamp.empresa.controllers;
+
+import cl.bootcamp.empresa.models.Cliente;
+
+public class AlmacenarClienteController {
+
+    public Cliente almacenarUsuarioValidar() {
+
+        return new Cliente();
+
+    }
+
+}
