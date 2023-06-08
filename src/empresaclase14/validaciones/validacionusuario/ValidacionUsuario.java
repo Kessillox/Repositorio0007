b121c8147a02f2f97f0e95868a7c49340f460448
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-06-08 11:11:07 -0400

    Validando Main para Usuario de nuevo...

--- src/empresaclase14/validaciones/validacionusuario/ValidacionUsuario.java
+++ src/empresaclase14/validaciones/validacionusuario/ValidacionUsuario.java
@@ -5,24 +5,32 @@ public class ValidacionUsuario {
     public static boolean esSoloLetras(String str) {
         for (int i = 0; i < str.length(); i++) {
             if (!Character.isLetter(str.charAt(i))) {
+                System.out.println("Solo debe ingresar letras.");
                 return false;
             }
         }
+        System.out.println("El String son solo letras esta bien");
         return true;
     }
 
     public static boolean validarNombre(String str) {
         if(str != null && str.length() >= 10 && str.length() <= 50){
-            System.out.println("Nombre ingresado correctamente!");
+            System.out.println("Nombre cumple el largo!");
             return true;
         }else{
-            System.out.println("El nombre no es valido");
+            System.out.println("Nombre no cumple el largo.");
             return false;
         }
     }
 
     public static boolean esStringObligatorio(String str) {
-        return (str != null && !str.isEmpty());
+        if(str != null && !str.isEmpty()){
+            System.out.println("Bien el nombre se esta!");
+            return true;
+        }else {
+            System.out.println("El ingreso debe ser obligatorio!");
+            return false;
+        }
     }
 
     public static boolean validarRun(long run){
