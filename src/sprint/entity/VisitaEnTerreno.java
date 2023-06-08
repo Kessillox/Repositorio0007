928a461c6b89c9def01efd4c9fcd8cceb978a483
sergioxiufu@gmail.com <106895461+xiu55@users.noreply.github.com>
2023-06-07 21:41:30 -0400

    cambio ultimo sergio

--- src/sprint/entity/VisitaEnTerreno.java
+++ src/sprint/entity/VisitaEnTerreno.java
@@ -5,5 +5,67 @@ public class VisitaEnTerreno {
     String dia;
     String hora;
     String lugar;
-    String Comentario;
+    String comentario;
+
+    public VisitaEnTerreno() {
+    }
+
+    public VisitaEnTerreno(int id, String dia, String hora, String lugar, String comentario) {
+        this.id = id;
+        this.dia = dia;
+        this.hora = hora;
+        this.lugar = lugar;
+        this.comentario = comentario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    @Override
+    public String toString() {
+        return "VisitaEnTerreno{" +
+                "id=" + dia +
+                ", dia='" + dia + '\'' +
+                ", hora='" + hora + '\'' +
+                ", lugar='" + lugar + '\'' +
+                ", comentario='" + comentario + '\'' +
+                '}';
+    }
 }
