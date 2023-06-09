4be55ac9535b5ce9f8dc547f4d9bcb786098538e
dayhip-ti <dayhip.ti@gmail.com>
2023-06-09 14:56:12 -0400

    Validacion de caracteres

--- src/Principal.java
+++ src/Principal.java
@@ -48,7 +48,7 @@ public class Principal {
                     System.out.println("¡Gracias por su preferencia <(n.n)>!");
                     break;
                 default:
-                    System.out.println("Opción inválida. Por favor, ingrese una opción válida: ");
+                    System.out.println("Opción inválida. Por favor, ingrese una opción válida: <(¬_¬)>");
                     break;
             }
         }
@@ -74,17 +74,20 @@ public class Principal {
         Cliente cli = new Cliente();
         System.out.println("Nombre Usuario: ");
         cli.setNombreUsuario(cadenaMinimoMaximoLenght(sc.nextLine(),10,50,
-                "Ingrese un nombre valido: "));
+                "Ingrese Nombre válido: ",true));
         System.out.println("Apellido Paterno: ");
-        cli.setApellido1(sc.nextLine());
+        cli.setApellido1(cadenaMinimoMaximoLenght(sc.nextLine(),5,30,
+                "Ingrese Apellido Paterno válido: ",true));
         System.out.println("Apellido Materno: ");
-        cli.setApellido2(sc.nextLine());
+        cli.setApellido2(cadenaMinimoMaximoLenght(sc.nextLine(),5,30,
+                "Ingrese Apellido Materno válido: ",true));
         System.out.println("Ingrese fecha de nacimiento: ");
         cli.setFechaDeNacimientoUsuario(sc.nextLine());
         System.out.println("Ingrese RUN: ");
         cli.setRunUsuario(sc.nextLine());
         System.out.println("Ingrese Nombre Empresa: ");
-        cli.setNombreEmpresa(sc.nextLine());
+        cli.setNombreEmpresa(cadenaMinimoMaximoLenght(sc.nextLine(),5,30,
+                "Ingrese Nombre Empresa válido: ",true));
         System.out.println("Ingrese Giro Empresa: ");
         cli.setGiroEmpresa(sc.nextLine());
         System.out.println("Ingrese RUT: ");
@@ -92,11 +95,13 @@ public class Principal {
         System.out.println("Ingrese Telefono: ");
         cli.setTelefonoRepresentanteLegal(sc.nextLine());
         System.out.println("Direccion Empresa: ");
-        cli.setDireccion(sc.nextLine());
+        cli.setDireccion(cadenaMinimoMaximoLenght(sc.nextLine(),0,70,
+                "Ingrese Direccion válida: ",false));
         System.out.println("Comuna: ");
-        cli.setComuna(sc.nextLine());
+        cli.setComuna(cadenaMinimoMaximoLenght(sc.nextLine(),0,50,
+                "Ingrese Comuna válida: ",true));
         contenedor.almacenarCliente(cli);
-
+        System.out.println("Registros agregados correctamente ~(^.^)~");
     }
 
     private static void agregarProfesional() {
@@ -104,20 +109,25 @@ public class Principal {
         System.out.println("---- Agregar Profesional ----");
         Profesional pro = new Profesional();
         System.out.println("Nombre Usuario: ");
-        pro.setNombreUsuario(sc.nextLine());
+        pro.setNombreUsuario(cadenaMinimoMaximoLenght(sc.nextLine(),10,50,
+                "Ingrese Nombre válido: ",true));
         System.out.println("Apellido Paterno: ");
-        pro.setApellido1(sc.nextLine());
+        pro.setApellido1(cadenaMinimoMaximoLenght(sc.nextLine(),5,30,
+                "Ingrese Apellido Paterno válido: ",true));
         System.out.println("Apellido Materno: ");
-        pro.setApellido2(sc.nextLine());
+        pro.setApellido2(cadenaMinimoMaximoLenght(sc.nextLine(),5,30,
+                "Ingrese Apellido Materno válido: ",true));
         System.out.println("Ingrese fecha de nacimiento: ");
         pro.setFechaDeNacimientoUsuario(sc.nextLine());
         System.out.println("Ingrese RUN: ");
         pro.setRunUsuario(sc.nextLine());
         System.out.println("Ingrese Titulo: ");
-        pro.setTitulo(sc.nextLine());
+        pro.setTitulo(cadenaMinimoMaximoLenght(sc.nextLine(),5,20,
+                "Ingrese Titulo válido: ",true));
         System.out.println("Ingrese la fecha de ingreso: ");
         pro.setFechaDeIngreso(sc.nextLine());
         contenedor.almacenarProfesional(pro);
+        System.out.println("Registros agregados correctamente ~(^.^)~");
     }
 
     private static void agregarAdministrativo() {
@@ -125,20 +135,26 @@ public class Principal {
         System.out.println("---- Agregar Administrativo ----");
         Administrativo adm = new Administrativo();
         System.out.println("Nombre Usuario: ");
-        adm.setNombreUsuario(sc.nextLine());
+        adm.setNombreUsuario(cadenaMinimoMaximoLenght(sc.nextLine(),10,50,
+                "Ingrese Nombre válido: ",true));
         System.out.println("Apellido Paterno: ");
-        adm.setApellido1(sc.nextLine());
+        adm.setApellido1(cadenaMinimoMaximoLenght(sc.nextLine(),5,30,
+                "Ingrese Apellido Paterno válido: ",true));
         System.out.println("Apellido Materno: ");
-        adm.setApellido2(sc.nextLine());
+        adm.setApellido2(cadenaMinimoMaximoLenght(sc.nextLine(),5,30,
+                "Ingrese Apellido Materno válido: ",true));
         System.out.println("Ingrese fecha de nacimiento: ");
         adm.setFechaDeNacimientoUsuario(sc.nextLine());
         System.out.println("Ingrese RUN: ");
         adm.setRunUsuario(sc.nextLine());
-        System.out.println("Ingrese Area: ");
-        adm.setArea(sc.nextLine());
+        System.out.println("Ingrese Área: ");
+        adm.setArea(cadenaMinimoMaximoLenght(sc.nextLine(),5,20,
+                "Ingrese Área válida: ",true));
         System.out.println("Ingrese Experiencia Previa: ");
-        adm.setExperienciaPrevia(sc.nextLine());
+        adm.setExperienciaPrevia(cadenaMinimoMaximoLenght(sc.nextLine(),0,100,
+                "Ingrese Experiencia Previa válida: ",false));
         contenedor.almacenarAdministrativo(adm);
+        System.out.println("Registros agregados correctamente ~(^.^)~");
     }
 
     private static void agregarCapacitacion() {
@@ -154,10 +170,12 @@ public class Principal {
         System.out.println("Ingrese Hora: ");
         cap.setHoraCapacitacion(sc.nextLine());
         System.out.println("Ingrese Lugar: ");
-        cap.setLugarCapacitacion(sc.nextLine());
-        System.out.println("Ingrese DUracion: ");
+        cap.setLugarCapacitacion(cadenaMinimoMaximoLenght(sc.nextLine(),10,50,
+                "Ingrese Lugar válido: ",true));
+        System.out.println("Ingrese Duracion: ");
         cap.setDuracionCapacitacion(sc.nextInt());
         contenedor.almacenarCapacitacion(cap);
+        System.out.println("Registros agregados correctamente ~(^.^)~");
     }
 
     private static void eliminarUsuario() {
@@ -167,11 +185,13 @@ public class Principal {
         System.out.println("Ingrese el RUN del usuario a eliminar: ");
         runn = sc.nextLine();
         contenedor.eliminarUsuario(runn);
+        System.out.println("Registros agregados correctamente ┌(u.u)┐ ");
     }
 
     private static void listarUsuarios() {
         System.out.println("---- Listar Usuarios ----");
         contenedor.listarUsuarios();
+        System.out.println("Son todos los registros ~(*o*)~");
     }
 
     private static void listarUsuariosPorTipo() {
@@ -191,16 +211,18 @@ public class Principal {
                     validar=true;
                     break;
                 default:
-                    System.out.println("Ingrese una opcion valida");
+                    System.out.println("Ingrese una opcion valida <(¬_¬)>");
                     break;
             }
         }
         contenedor.listarUsuariosPorTipo(tipo);
+        System.out.println("Son todos los registros ~(*o*)~");
     }
 
     private static void listarCapacitaciones() {
         System.out.println("---- Listar Capacitaciones ----");
         contenedor.listarCapacitaciones();
+        System.out.println("Son todos los registros ~(*o*)~");
     }
 
     private static int validarInt(String mensaje) {
@@ -218,16 +240,24 @@ public class Principal {
     }
 
     /**********Validaciones***********/
-    private static String cadenaMinimoMaximoLenght (String cadena, int min, int max,String mensajeError) {
-        while (cadena.length() < min || cadena.length() > max) {
-            if (cadena.length() < min) {
-                System.out.println("Como mínimo " + min + " caracteres.");
-            } else {
-                System.out.println("Como máximo " + max + " caracteres.");
-            }
-            System.out.println(mensajeError);
+    private static String cadenaMinimoMaximoLenght (String cadena, int min, int max,String mensajeError,boolean obligatorio) {
+        if (obligatorio){
+            while (cadena.length() < min || cadena.length() > max) {
+                if (cadena.length() < min) {
+                    System.out.println("Como mínimo " + min + " caracteres.");
+                } else {
+                    System.out.println("Como máximo " + max + " caracteres.");
+                }
+                System.out.println(mensajeError+" <(¬_¬)>");
 
-            cadena = sc.nextLine();
+                cadena = sc.nextLine();
+            }
+        }else{
+            while ( cadena.length() > max) {
+                System.out.println("Como máximo " + max + " caracteres.");
+                System.out.println(mensajeError+" <(¬_¬)>");
+                cadena = sc.nextLine();
+            }
         }
         return cadena;
     }
