435d06939ebeb25c4591bdf865266302e6a6db61
Cristian <duranferreira.cristian@gmail.com>
2023-06-09 12:23:57 -0400

    actualización clase Cotroller Administrativo y Profesional, se agregan métodos de Usuario

--- src/cl/bootcamp/empresa/controllers/AlmacenarAdministrativoController.java
+++ src/cl/bootcamp/empresa/controllers/AlmacenarAdministrativoController.java
@@ -3,6 +3,8 @@ package cl.bootcamp.empresa.controllers;
 import cl.bootcamp.empresa.models.Administrativo;
 import cl.bootcamp.empresa.models.Profesional;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class AlmacenarAdministrativoController {
@@ -11,24 +13,81 @@ public class AlmacenarAdministrativoController {
 
     Administrativo administrativo = new Administrativo();
 
-    public Administrativo almacenarAdministrativoController(){
+    public Administrativo almacenarAdministrativoController() {
+
+        // ======================= Validaciones Usuario =======================
+        System.out.println("(obligatorio) Ingresar nombre: ");
+        while (true) {
+            administrativo.setNombre(sc.nextLine().trim());
+            if (administrativo.getNombre().length() < 10 || administrativo.getNombre().length() > 50) {
+                System.err.println("Error, ingresar nombre de 10 a 50 caracteres");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("(obligatorio) Ingresar primer apellido: ");
+        while (true) {
+            administrativo.setApellido1(sc.nextLine().trim());
+            if (administrativo.getApellido1().length() < 5 || administrativo.getApellido1().length() > 30) {
+                System.err.println("Error, ingresar apellido de 5 a 30 caracteres");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("(obligatorio) Ingresar segundo apellido: ");
+        while (true) {
+            administrativo.setApellido2(sc.nextLine().trim());
+            if (administrativo.getApellido2().length() < 5 || administrativo.getApellido2().length() > 30) {
+                System.err.println("Error, ingresar apellido de 5 a 30 caracteres");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("(obligatorio) Ingresar fecha de nacimiento con formato dd/MM/yyyy: ");
+        while (true) {
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                administrativo.setFechaNacimiento(df.format(df.parse(sc.next())));
+                break;
+            } catch (ParseException e) {
+                System.err.println("La fecha tiene un formato incorrecto");
+                System.err.println("El formato debe ser dd/MM/yyyy");
+            }
+        }
+
+        System.out.println("(obligatorio) Ingresar RUN con el formato 00000000-k: ");
+        while (true) {
+            String runIngresado = sc.next();
+
+            if (runIngresado.matches("\\d{8}-[\\d|kK]")) {
+                administrativo.setRun(runIngresado);
+                break;
+            } else {
+                System.err.println("El RUN ingresado no cumple con el formato requerido.");
+            }
+        }
+        sc.nextLine(); // Consumir el salto de línea
 
         // ======================= Validaciones Profesional =======================
         System.out.println("(obligatorio) Ingresar área: ");
         while (true) {
             administrativo.setArea(sc.nextLine().trim());
             if (administrativo.getArea().length() < 5 || administrativo.getArea().length() > 20) {
-                System.out.println("Error, ingresar experencia previa entre 5 y 20 caracteres");
+                System.err.println("Error, ingresar experencia previa entre 5 y 20 caracteres");
             } else {
                 break;
             }
         }
 
-        System.out.println("(obligatorio) Ingresar experiencia previa: ");
+        System.out.println("(opcional) Ingresar experiencia previa: ");
         while (true) {
             administrativo.setExperenciaArea(sc.nextLine().trim());
+            if (administrativo.getExperenciaArea().isBlank()) break;
             if (administrativo.getExperenciaArea().length() > 100) {
-                System.out.println("Error, ingresar título profesional máximo 100 caracteres");
+                System.err.println("Error, ingresar título administrativo máximo 100 caracteres");
             } else {
                 break;
             }
--- src/cl/bootcamp/empresa/controllers/AlmacenarProfesionalController.java
+++ src/cl/bootcamp/empresa/controllers/AlmacenarProfesionalController.java
@@ -14,22 +14,42 @@ public class AlmacenarProfesionalController {
 
     public Profesional almacenarProfesionalController() {
 
-        // ======================= Validaciones Profesional =======================
-        System.out.println("(obligatorio) Ingresar título profesional: ");
+        // ======================= Validaciones Usuario =======================
+        System.out.println("(obligatorio) Ingresar nombre: ");
         while (true) {
-            profesional.setTitulo(sc.nextLine().trim());
-            if (profesional.getTitulo().length() > 70) {
-                System.out.println("Error, ingresar título profesional de máximo 70 caracteres");
+            profesional.setNombre(sc.nextLine().trim());
+            if (profesional.getNombre().length() < 10 || profesional.getNombre().length() > 50) {
+                System.err.println("Error, ingresar nombre de 10 a 50 caracteres");
             } else {
                 break;
             }
         }
 
-        System.out.println("(obligatorio) Ingresar fecha de ingreso con formato dd/MM/yyyy: ");
+        System.out.println("(obligatorio) Ingresar primer apellido: ");
+        while (true) {
+            profesional.setApellido1(sc.nextLine().trim());
+            if (profesional.getApellido1().length() < 5 || profesional.getApellido1().length() > 30) {
+                System.err.println("Error, ingresar apellido de 5 a 30 caracteres");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("(obligatorio) Ingresar segundo apellido: ");
+        while (true) {
+            profesional.setApellido2(sc.nextLine().trim());
+            if (profesional.getApellido2().length() < 5 || profesional.getApellido2().length() > 30) {
+                System.err.println("Error, ingresar apellido de 5 a 30 caracteres");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("(obligatorio) Ingresar fecha de nacimiento con formato dd/MM/yyyy: ");
         while (true) {
             SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
             try {
-                profesional.setFechaIngreso(df.format(df.parse(sc.next())));
+                profesional.setFechaNacimiento(df.format(df.parse(sc.next())));
                 break;
             } catch (ParseException e) {
                 System.err.println("La fecha tiene un formato incorrecto");
@@ -37,6 +57,48 @@ public class AlmacenarProfesionalController {
             }
         }
 
+        System.out.println("(obligatorio) Ingresar RUN con el formato 00000000-k: ");
+        while (true) {
+            String runIngresado = sc.next();
+
+            if (runIngresado.matches("\\d{8}-[\\d|kK]")) {
+                profesional.setRun(runIngresado);
+                break;
+            } else {
+                System.err.println("El RUN ingresado no cumple con el formato requerido.");
+            }
+        }
+        sc.nextLine(); // Consumir el salto de línea
+
+        // ======================= Validaciones Profesional =======================
+        System.out.println("(obligatorio) Ingresar título profesional: ");
+        while (true) {
+            profesional.setTitulo(sc.nextLine().trim());
+            if (profesional.getTitulo().length() > 70) {
+                System.err.println("Error, ingresar título profesional de máximo 70 caracteres");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("(opcional) Ingresar fecha de ingreso con formato dd/MM/yyyy: ");
+        while (true) {
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            String input = sc.next();
+
+            if (input.isBlank()) {
+                break;
+            }
+
+            try {
+                df.parse(input);
+                profesional.setFechaIngreso(input);
+                break;
+            } catch (ParseException e) {
+                System.err.println("La fecha tiene un formato incorrecto");
+                System.err.println("El formato debe ser dd/MM/yyyy");
+            }
+        }
         return profesional;
     }
 }
