c611a0e279f4a19275634ad7648744cc4d584200
Livio <livio.gutierrez.v@gmail.com>
2023-06-09 15:37:52 -0400

    Siguiendo con el trabajo 25

---
+++ src/trabajo25/Principal.java
@@ -0,0 +1,17 @@
+package trabajo25;
+
+public class Principal {
+    public static void main(String[] args) {
+        //Variables
+        String nombre=null;
+        String apellido1=null;
+        String apellido2=null;
+        String run=null;
+        int telefono=0;
+        int edad=0;
+
+        Trabajador trabajador = new Trabajador(nombre,apellido1,apellido2,run,telefono,edad);
+
+
+    }
+}
