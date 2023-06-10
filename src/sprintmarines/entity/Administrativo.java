8189c09b1b33d5249edb0e9e8a59e7d56b599378
Leandro Villalba <leandrovillalba.dev>
2023-06-09 21:58:47 -0400

    Refactorizando packages, actualizando Javadocs, eliminando clases de ejercicios pasados

---
+++ src/sprintmarines/entity/Administrativo.java
@@ -0,0 +1,73 @@
+package sprintmarines.entity;
+
+/*
+  PRINCIPIO ABIERTO/CERRADO
+
+  Esta clase extiende desde la clase usuario, es decir hereda
+  a su vez sus atributos están privados por lo que se puede amplicar sin modificar su
+  implementación existente.
+ */
+
+/**
+ * Clase que representa un administrativo, que extiende la clase Usuario.
+ * Contiene información adicional sobre el área y experiencia previa del administrativo.
+ * También tiene un método para analizar la información del administrativo.
+ *
+ * @author Leadro Villalba
+ * @author Luis Zambrano
+ * @author Gustavo Vargas
+ * @author Cristian Trureo
+ * @version 1.2
+ */
+public class Administrativo extends Usuario {
+    private String area;
+    private String experienciaPrevia;
+
+    public Administrativo() {
+    }
+
+    public Administrativo(String area, String experienciaPrevia) {
+        this.area = area;
+        this.experienciaPrevia = experienciaPrevia;
+    }
+
+    public Administrativo(String nombre, String apellido1, String apellido2, String fechaDeNacimiento, Integer rut, String area, String experienciaPrevia) {
+        super(nombre, apellido1, apellido2, fechaDeNacimiento, rut);
+        this.area = area;
+        this.experienciaPrevia = experienciaPrevia;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getExperienciaPrevia() {
+        return experienciaPrevia;
+    }
+
+    public void setExperienciaPrevia(String experienciaPrevia) {
+        this.experienciaPrevia = experienciaPrevia;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrativo{" +
+                "area='" + area + '\'' +
+                ", experienciaPrevia='" + experienciaPrevia + '\'' +
+                '}';
+    }
+
+    /**
+     * Método que analiza la información del administrativo e imprime por pantalla el área y
+     * experiencia previa del mismo.
+     */
+    @Override
+    public void analizarUsuario() {
+        super.analizarUsuario();
+        System.out.println("     Área: " + this.getArea() + ". Experiencia Previa: " + this.getExperienciaPrevia());
+    }
+}
---
+++ src/sprintmarines/validaciones/validacionusuario/ValidacionUsuario.java
@@ -0,0 +1,55 @@
+package sprintmarines.validaciones.validacionusuario;
+
+public class ValidacionUsuario {
+
+    public static boolean esSoloLetras(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isLetter(str.charAt(i))) {
+                System.out.println("Solo puedes ingresar letras.");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean validarNombre(String str) {
+        if(str != null && str.length() >= 3 && str.length() <= 50){
+            return true;
+        }else{
+            System.out.println("Inserte entre 3 y 50 carácteres");
+            return false;
+        }
+    }
+
+    public static boolean validarApellido(String str) {
+        if(str != null && str.length() >= 3 && str.length() <= 30){
+            return true;
+        }else{
+            System.out.println("Inserte entre 3 y 30 carácteres");
+            return false;
+        }
+    }
+
+    public static boolean esStringObligatorio(String str) {
+        if(str != null && !str.isEmpty()){
+            return true;
+        }else {
+            System.out.println("El ingreso debe ser obligatorio!");
+            return false;
+        }
+    }
+
+    public static boolean validarRunUser(int rut){
+        if(rut < 99999999) {
+            System.out.println("RUN Usuario registrado correctamente.");
+            return true;
+        }else{
+            System.out.println("RUT Usuario ingresado incorrectamente.");
+            return false;
+        }
+    }
+
+
+
+}
