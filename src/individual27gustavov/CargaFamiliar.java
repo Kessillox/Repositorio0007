a8a19234029397c88cbfc9d76d5669db7e612e9e
StudioMontt <electri.mv@gmail.com>
2023-06-10 23:37:18 -0400

    resubiendo ejercicio 27

---
+++ src/individual27gustavov/CargaFamiliar.java
@@ -0,0 +1,137 @@
+package individual27gustavov;
+
+import java.util.Scanner;
+
+/**
+ * Individual 27: Crear una clase CargaFamiliar creando algunos metodos.
+ *
+ * @author Gustavo Vargas
+ * @version 1.0
+ */
+public class CargaFamiliar {
+    private String runCarga;
+    private String nombres;
+    private String apellidos;
+    private String runFamiliar;
+    private int parentesco;
+    private int edad;
+
+    // Constructor vacío
+    public CargaFamiliar() {
+    }
+
+    /**
+     * Constructor con parámetros
+     *
+     * @param nombres
+     * @param apellidos
+     * @param "Run       del" trabajador
+     * @param parentesco
+     * @param edad
+     */
+    public CargaFamiliar(String runCarga, String nombres, String apellidos, Trabajador trabajador, int parentesco, int edad) {
+        this.runCarga = runCarga;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.runFamiliar = trabajador.getRun();
+        this.parentesco = parentesco;
+        this.edad = edad;
+    }
+
+    // Métodos Get y Set de los parámetros
+    public String getRunCarga() {
+        return runCarga;
+    }
+
+    public void setRunCarga(String runCarga) {
+        this.runCarga = runCarga;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getRunFamiliar() {
+        return runFamiliar;
+    }
+
+    public void setRunFamiliar(String runFamiliar) {
+        this.runFamiliar = runFamiliar;
+    }
+
+    public int getParentesco() {
+        return parentesco;
+    }
+
+    public void setParentesco(int parentesco) {
+        this.parentesco = parentesco;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "CargaFamiliar{" +
+                "\n runCarga= '" + runCarga + "'," +
+                "\n nombres= '" + nombres + "'," +
+                "\n apellidos= '" + apellidos + "'," +
+                "\n runFamiliar= '" + runFamiliar + "'," +
+                "\n parentesco= " + parentesco + "," +
+                "\n edad= " + edad + "," +
+                "\n}";
+    }
+
+    public int ingresarParentesco() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int parentesco = 0;
+
+        System.out.println("Ingrese el parentesco de la carga familiar:");
+        System.out.println("1. Cónyuge");
+        System.out.println("2. Hijo/a");
+        System.out.println("3. Otro");
+
+        do {
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); // Realizar salto de línea después de la opción
+
+            switch (opcion) {
+                case 1:
+                    parentesco = 1; //"Cónyuge"
+                    break;
+                case 2:
+                    parentesco = 2; //"Hijo/a"
+                    break;
+                case 3:
+                    parentesco = 3; //"Otro"
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente nuevamente.");
+                    break;
+            }
+        } while (parentesco == 0);
+
+        // Asignar el valor del parentesco a la variable de instancia
+        this.parentesco = parentesco;
+        return parentesco;
+    }
+}
---
+++ src/individual27gustavov/Main.java
@@ -0,0 +1,38 @@
+package individual27gustavov;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de Trabajador
+        Trabajador trabajador = new Trabajador("Marcelo", "Vargas Pérez", "13456789-6",
+                "123456789", 40);
+        System.out.println();
+
+        // Probar los métodos de la clase Trabajador
+        System.out.println("Nombre completo: " + trabajador.nombreCompleto());
+        System.out.println("Edad: " + trabajador.getEdad());
+        System.out.println("RUN descompuesto: " + trabajador.descomponerRun());
+        System.out.println();
+
+        // Modificar algunos atributos utilizando los métodos set
+        trabajador.setNombres("Gustavo");
+        trabajador.setEdad(45);
+
+        // Mostrar la representación en cadena del objeto utilizando el método toString
+        System.out.println(trabajador.toString());
+        System.out.println();
+
+        // Ingreso del parentesco de la carga familiar
+        CargaFamiliar carga = new CargaFamiliar();
+        int parentesco = carga.ingresarParentesco();
+
+        // Crear una instancia de CargaFamiliar utilizando los datos ingresados
+        CargaFamiliar carga1 = new CargaFamiliar("25123456-7", "Catalina", "Vargas Barde",
+                trabajador, parentesco, 4);
+        System.out.println();
+
+        // Mostrar los datos de la carga familiar
+        System.out.println("Datos de la " + carga1.toString());
+    }
+}
