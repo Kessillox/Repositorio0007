7358ed3466cbce1a1818b53e9687e64240f830f5
Jucame <jcmerino@uc.cl>
2023-06-14 19:47:50 -0400

    individual31 comienza

---
+++ src/individual31/CargaFamiliar.java
@@ -0,0 +1,124 @@
+package individual31;
+
+/**
+ * Clase que almacena la informacion de la carga familiar
+ * @author Juan Merino
+ * @version 1.0
+ */
+public class CargaFamiliar {
+
+    private String runCarga, nombresCarga, apellidosCarga, runFamiliar;
+    private int parentesco, edad;
+
+    /**
+     * Método que entrega la informacion de la clase ordenada y legible
+     * @return String con la informacion de la clase
+     */
+    @Override
+    public String toString() {
+        String[] opcionesParentesco = {"Cónyuge", "Hijo/a", "Otro"};
+        return "\nCarga Familiar\n" +
+                "RUN carga: " + runCarga + "\n" +
+                "Nombres: " + nombresCarga + "\n" +
+                "Apellidos: " + apellidosCarga + "\n" +
+                "RUN Familiar: " + runFamiliar + "\n" +
+                "Parentesco" + opcionesParentesco[parentesco - 1] + "\n" +
+                "Edad: " + edad + "\n";
+    }
+
+    /**
+     * Getter de atributo privado runCarga
+     * @return String runCarga almacenada en la clase
+     */
+    public String getRunCarga() {
+        return runCarga;
+    }
+
+    /**
+     * Setter de atributo privado runCarga
+     * @param runCarga String del nuevo RUN a almacenar
+     */
+    public void setRunCarga(String runCarga) {
+        this.runCarga = runCarga;
+    }
+
+    /**
+     * Getter de atributo privado nombresCarga
+     * @return String del atributo almacenado en la clase
+     */
+    public String getNombresCarga() {
+        return nombresCarga;
+    }
+
+    /**
+     * Setter del atributo privado nombresCarga
+     * @param nombresCarga nombre de la carga
+     */
+    public void setNombresCarga(String nombresCarga) {
+        this.nombresCarga = nombresCarga;
+    }
+
+    /**
+     * Getter del atributo privado apellidoscarga
+     * @return apellidos almacenados en la clase
+     */
+    public String getApellidosCarga() {
+        return apellidosCarga;
+    }
+
+    /**
+     * Setter del atributo que almacena apellidos
+     * @param apellidosCarga String con los apellidos de la carga
+     */
+    public void setApellidosCarga(String apellidosCarga) {
+        this.apellidosCarga = apellidosCarga;
+    }
+
+    /**
+     * Getter del atributo que almacena el run familiar
+     * @return String con a información almacenada del run familiar
+     */
+    public String getRunFamiliar() {
+        return runFamiliar;
+    }
+
+    /**
+     * Setter del atributo que almacena el run familiar
+     * @param runFamiliar String con el run familiar a almacenar
+     */
+    public void setRunFamiliar(String runFamiliar) {
+        this.runFamiliar = runFamiliar;
+    }
+
+    /**
+     * Getter del atributo que almacena el parentesco
+     * @return int con relacion de parentesco 1) conyugue 2) hijo/a 3) otro
+     */
+    public int getParentesco() {
+        return parentesco;
+    }
+
+    /**
+     * Setter del atributo que almacena el parentesco
+     * @param parentesco 1 para conyugue, 2 para hijo/a, 3 para otro
+     */
+    public void setParentesco(int parentesco) {
+        this.parentesco = parentesco;
+    }
+
+    /**
+     * Getter del atributo privado que almacena la edad
+     * @return int con la edad de la carga
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Setter del atributo privado que almacena la edad
+     * @param edad int con la edad de la carga
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
---
+++ src/individual31/Trabajador.java
@@ -0,0 +1,107 @@
+package individual31;
+
+/**
+ * Clase que almacena la informacion de cada trabajador
+ * @author Juan Merino
+ * @version 2.0
+ */
+public class Trabajador {
+
+    private String nombres, apellidos, run, telefono;
+    private int edad;
+
+    /**
+     * Método toString() sobreescrito
+     * @return String con la informacion del trabajador ordenada y legible
+     */
+    @Override
+    public String toString() {
+        return "Trabajador\n" +
+                "Nombres: " + nombres + "\n" +
+                "Apellidos: " + apellidos + "\n" +
+                "RUN: " + run + "\n" +
+                "Telefono: " + telefono + "\n" +
+                "Edad: " + edad + "\n";
+    }
+
+    /**
+     * Constructor vacio
+     */
+    public Trabajador() {
+    }
+
+    /**
+     * Constructor que recibe los atributos como parametros
+     * @param nombres   Nombres del trabajador, String
+     * @param apellidos Apellidos del trabajador, String
+     * @param run       RUN del trabajador debe contener guion, String
+     * @param telefono  Telefono del trabajador, String
+     * @param edad      Edad del trabajador, int
+     */
+    public Trabajador(String nombres, String apellidos, String run, String telefono, int edad) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.run = run;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    /**
+     * Metodo que concatena los nombres y apellidos del trabajador
+     * @return String con el nombre completo del trabajador
+     */
+    public String nombreCompleto() {
+        return nombres + " " + apellidos;
+    }
+
+    /**
+     * Metodo que descompone el RUN del trabajador, debe contener un guion para hacer la separacion
+     * @return int anterior al guion, 0 en caso que no haya un guion en el RUN
+     */
+    public int descomponerRun() {
+        if (run.contains("-")) {
+            return Integer.parseInt(run.split("-")[0]);
+        }
+        return 0; // no contiene "-"
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
