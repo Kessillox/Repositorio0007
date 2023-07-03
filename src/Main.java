8f4af5973e44ce257c550eb2eea297f3e1a93e38
Kessi <kessi@koders.cl>
2023-07-03 15:57:19 -0400

    ajuste Sonar Lint

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import mascotas.Perro;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal1, animal2;
+        Animal animal1;
+        Animal animal2;
         animal1= new Perro("Canino", "Fido");
         animal2= new Gato("Felino", "Snarf");
 
